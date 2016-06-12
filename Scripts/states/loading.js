5dc08607f3e75e95781c61170dc64f0ba5a08ae8
Willy Barro <willybarro@gmail.com>
2016-06-11 22:46:21 -0300

    Remove unused files

--- Scripts/states/loading.js
+++
@@ -1,8 +0,0 @@
-states.loading = {
-    preload: function(){
-        var loadingLabel = game.add.text(600, 550, 'Loading...', { font: '30px Courier', fill: '#ffffff' });
-
-        gamePreload();
-    },
-    create: gameCreate
-};
